630776a83b65ab4fc10d1ca9064cf857dabf230f
Nhi Nguyen <nnguy152@gmail.com>
2018-03-27 10:10:53 -0400

    initial commit

---
+++ src/Nav/Nav.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class Nav extends Component {
+  render() {
+    return (
+      <nav>
+        <button className="red">Red</button>
+        <button className="grey">Grey</button>
+        <button className="green">Green</button>
+      </nav>
+    )
+  }
+}
+
+export default Nav
