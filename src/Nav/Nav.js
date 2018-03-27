397795d422508b3ab6c1a51dee9361215b13bb11
Nhi Nguyen <nnguy152@gmail.com>
2018-03-27 13:34:19 -0400

    add style and link area

--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -1,17 +1,54 @@
-import React, { Component } from 'react'
+import React from 'react'
+import './Nav.css'
 
-class Nav extends Component {
-  render() {
-    return (
-      <nav>
-        <button className="red">Red</button>
-        <button className="grey">Grey</button>
-        <button className="green">Green</button>
-        <div className="link">Link</div>
-        <button>Comments</button>
-      </nav>
-    )
-  }
+const Nav = () => {
+  return (
+    <nav>
+      <div>
+        <button className="red left-btns" id="left-btn-container">
+          <span className="left-btn-text">
+            <img
+              className="left-btn-img"
+              src={require('./thumbsup.png')}
+              alt="LIK"
+            />
+          </span>
+        </button>
+        <button className="grey left-btns" id="left-btn-container">
+          <span className="left-btn-text">
+            <img
+              className="left-btn-img"
+              src={require('./thumbsup.png')}
+              alt="LIK"
+            />
+          </span>
+        </button>
+        <button className="green left-btns" id="left-btn-container">
+          <span className="left-btn-text">
+            <img
+              className="left-btn-img-down"
+              src={require('./thumbsdown.png')}
+              alt="LIK"
+            />
+          </span>
+        </button>
+      </div>
+      <div>
+        <input
+          className="link link-div"
+          id="foo"
+          value="wwwejwifejoaigrghrsk;gn;lrsjgl;asrjg;lsjglsjrgl;djrg;dsj;lgdajgl;aj;fjr;jsd;j;cs;jsfgrgergwtghretglrskdjga;elrkjg;lerjg;erhg;lher;gher;lg;elwrhg;erhger;"
+        />
+      </div>
+      <div>
+        <button className="wobble-btn">
+          <div id="main">WOBBLE</div>
+        </button>
+
+        <button className="comment-btn">Comments</button>
+      </div>
+    </nav>
+  )
 }
 
 export default Nav
