97c6715f9061841ad1d511d3ad0ad038093c3424
Nhi Nguyen <nnguy152@gmail.com>
2018-03-28 16:01:50 -0400

    test

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
+import HomePage from './HomePage/HomePage'
 import { BrowserRouter as Router } from 'react-router-dom'
 import registerServiceWorker from './registerServiceWorker'
 
 ReactDOM.render(
   <Router>
-    <App />
+    <HomePage />
   </Router>,
   document.getElementById('root')
 )
